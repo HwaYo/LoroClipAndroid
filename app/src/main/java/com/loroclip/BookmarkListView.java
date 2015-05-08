0e2c2c5045cdd2a868a691729086edef9b858aa1
minhyeok4dev <minhyeok4dev@gmail.com>
2015-05-08 19:52:51 +0900

    init loroclip

---
+++ app/src/main/java/com/loroclip/BookmarkListView.java
@@ -0,0 +1,15 @@
+package com.loroclip;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by minhyeok on 5/6/15.
+ */
+public class BookmarkListView extends ListView {
+
+    public BookmarkListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/loroclip/BookmarkMap.java
@@ -0,0 +1,14 @@
+package com.loroclip;
+
+import java.util.HashMap;
+
+/**
+ * Created by minhyeok on 5/8/15.
+ */
+public class BookmarkMap extends HashMap<String, Integer> {
+    public BookmarkMap() {
+        put("important", 0xffe56673);
+        put("i dont know", 0xffff66ff);
+        put("great", 0xFF90FFB0);
+    }
+}
