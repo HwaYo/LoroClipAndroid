9ab8797c380843df7f0946b45cf849d7955e9a84
stompesi <stompesi@gmail.com>
2015-05-17 17:46:21 +0900

    record list view add

--- app/src/main/java/com/loroclip/record/RecordListActivity.java
+++ app/src/main/java/com/loroclip/record/RecordListActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,12 +21,60 @@ import java.util.List;
 
 public class RecordListActivity extends Activity {
 
-  ListView recordListView;
+  private final String LOROCLIP_PATH = Environment.getExternalStorageDirectory().toString() + "/Loroclip/";;
+  private final String AUDIO_OGG_EXTENSION = ".ogg";
+
+  private ListView recordListView;
+  private AdapterView.OnItemClickListener selectItemListener = new AdapterView.OnItemClickListener() {
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+      String  fileName    = (String) recordListView.getItemAtPosition(position);
+
+      String filePath = LOROCLIP_PATH + fileName + AUDIO_OGG_EXTENSION;
+
+      try {
+        Intent intent = new Intent(Intent.ACTION_EDIT, Uri.parse(filePath));
+        intent.putExtra("was_get_content_intent", true);
+        intent.setClassName( "com.loroclip", "com.loroclip.LoroClipEditActivity");
+        startActivityForResult(intent, 1);
+      } catch (Exception e) {
+        Log.e("LoroClip", "Couldn't start editor");
+      }
+    }
+  };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_record_list);
+
+    recordListView = (ListView) findViewById(R.id.recordListView);
+
+    setRecordListItem();
+    addEvnetListener();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_record_list, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+    if (id == R.id.action_settings) {
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  public void addEvnetListener() {
+
+    recordListView.setOnItemClickListener(selectItemListener);
+  }
+
+  public void setRecordListItem() {
 
     List<Record> records = Record.listAll(Record.class);
     ArrayList<String> titles = new ArrayList<String>();
@@ -37,49 +86,7 @@ public class RecordListActivity extends Activity {
     String[] titleArray = new String[titles.size()];
     titles.toArray(titleArray);
 
-
-    recordListView = (ListView) findViewById(R.id.recordListView);
-    recordListView.setAdapter(new ArrayAdapter<String>(
-        this, android.R.layout.simple_list_item_single_choice, android.R.id.text1, titleArray));
-    recordListView.setOnItemClickListener(testListener);
+    recordListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, android.R.id.text1, titleArray));
   }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_record_list, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
-
-  private AdapterView.OnItemClickListener testListener = new AdapterView.OnItemClickListener() {
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-      String  fileName    = (String) recordListView.getItemAtPosition(position);
-
-      String filename = "/storage/emulated/0/Loroclip/" + fileName + ".wav";
-        try {
-          Intent intent = new Intent(Intent.ACTION_EDIT, Uri.parse(filename));
-          intent.putExtra("was_get_content_intent", true);
-          intent.setClassName( "com.loroclip", "com.loroclip.LoroClipEditActivity");
-          startActivityForResult(intent, 1);
-        } catch (Exception e) {
-        Log.e("LoroClip", "Couldn't start editor");
-        }
-    }
-  };
 }
