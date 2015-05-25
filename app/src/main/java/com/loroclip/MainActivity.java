b2d7c7c0fc07e3a85797a65bbf9587abfef98bb9
Hong ChulJu <ang0123dev@gmail.com>
2015-05-25 21:52:59 +0900

    Records should be sorted by created_at desc.

--- app/src/main/java/com/loroclip/MainActivity.java
+++ app/src/main/java/com/loroclip/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends ActionBarActivity implements RecordListAdapter
         setContentView(R.layout.activity_material_list);
         setSyncAutomatic();
 
-        mRecords = Record.listExists(Record.class, "created_at DESC");
+        mRecords = Record.listExists(Record.class);
 
         // Android L Style Title Bar
         mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
